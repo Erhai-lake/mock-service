0d07370d4795c37805fa5530cc1f56968f34d866
Erhai-lake <erhai.lake.0714@gmail.com>
2026-01-26 01:51:58 +0800

    refactor(registries): 导出类型定义以支持外部使用
    - 将 CategoryRegistry 和 ProcessorCategoryRegistry 类型导出
    - 使得外部模块可以引用这些类型定义
    - 提高代码的可复用性和类型安全性

--- src/registries/index.ts
+++ src/registries/index.ts
@@ -1,6 +1,8 @@
 import {CategoryRegistry} from "./CategoryRegistry"
 import {ProcessorCategoryRegistry} from "./ProcessorCategoryRegistry"
 
+export type {CategoryRegistry, ProcessorCategoryRegistry}
+
 export function createCategoryRegistry() {
 	return new CategoryRegistry()
 }
